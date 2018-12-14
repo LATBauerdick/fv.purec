71c09353c91c0bc45f862fc70dc334a7c43fed56
LATBauerdick <github@latbauerdick.de>
2018-12-14 00:21:41 -0600

    Test.Input works

---
+++ src/Data/String/Utils.purs
@@ -0,0 +1,55 @@
+module Data.String.Utils where
+-- from https://github.com/cdepillabout/purescript-words-lines/blob/master/src/Data/Array/WordsLines.purs
+import Prelude (not, (<<<), map, (==), ($), (<>), (<=), (>=), (||), (&&))
+
+import Data.Array as Array
+import Data.Foldable (foldl)
+import Data.Maybe (Maybe(..))
+import Data.String as String
+import Data.Char ( toCharCode )
+
+-- | `words` breaks a string up into a list of words, which were delimited
+-- | by white space.
+-- |
+-- | ```purescript
+-- | > words ""
+-- | [""]
+-- | > words "hello"
+-- | ["hello"]
+-- | > words "hello     "
+-- | ["hello"]
+-- | > words "   foo     bar   "
+-- | ["foo", "bar"]
+-- | > words "foo\t\nbar"
+-- | ["foo", "bar"]
+-- | ```
+words :: String -> Array String
+words = map String.fromCharArray <<< go <<< String.toCharArray
+  where
+    go :: Array Char -> Array (Array Char)
+    go s =
+        case Array.uncons $ Array.dropWhile isSpace s of
+            Nothing -> []
+            Just { head: head, tail: tail } ->
+                let withBreaks = break isSpace (head `Array.cons` tail)
+                in withBreaks.init `Array.cons` go withBreaks.rest
+
+-- | `break p as` is `span (not <<< p) as`.
+break :: forall a . (a -> Boolean) -> Array a -> { init :: (Array a), rest :: (Array a) }
+break p = Array.span (not <<< p)
+
+{-- import Data.Char.Unicode (isSpace) --}
+-- | Returns `True` for any Unicode space character, and the control
+-- | characters `\t`, `\n`, `\r`, `\f`, `\v`.
+-- |
+-- | `isSpace` includes non-breaking space.
+isSpace :: Char -> Boolean
+-- The magic 0x377 used in the code below isn't really that magical. As of
+-- 2014, all the codepoints at or below 0x377 have been assigned, so we
+-- shouldn't have to worry about any new spaces appearing below there.
+isSpace c = if uc <= 0x337
+               then uc == 32 || (uc >= 9 && uc <= 13) || uc == 0xa0
+               else false --???????? uIswspace $ toCharCode -- this is from purescript-unicode
+  where
+    uc :: Int
+    uc = toCharCode c
---
+++ src/Global.c
@@ -0,0 +1,11 @@
+#include <purescript.h>
+
+PURS_FFI_FUNC_1(Global_readFloat, x, {
+  double xx = atof(purs_any_get_string(x));
+  return purs_any_num_new(xx);
+});
+
+PURS_FFI_FUNC_1(Global_isFinite, x, {
+  double xx = purs_any_get_num(x);
+  return purs_any_bool(isfinite(xx));
+});
