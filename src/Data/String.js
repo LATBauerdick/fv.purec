9bc4ea8a0cffe5f7ef9d650fdbd6925c41336935
LATBauerdick <github@latbauerdick.de>
2018-12-15 19:11:02 -0600

    Format works

--- src/Data/String.js
+++ src/Data/String.js
@@ -1,10 +1,39 @@
 "use strict";
 
-exports.fromCharArrayImpl = function (a) {
+exports.fromCharArray = function (a) {
   return a.join("");
 };
 
-exports.lengthImpl = function (s) {
+exports.length = function (s) {
   return s.length;
 };
 
+exports.toCharArray = function (s) {
+  return s.split("");
+};
+
+exports.singleton = function (c) {
+  return c;
+};
+
+exports.countPrefix = function (p) {
+  return function (s) {
+    var i = 0;
+    while (i < s.length && p(s.charAt(i))) i++;
+    return i;
+  };
+};
+
+exports.drop = function (n) {
+  return function (s) {
+    return s.substring(n);
+  };
+};
+
+exports.replace = function (s1) {
+  return function (s2) {
+    return function (s3) {
+      return s3.replace(s1, s2);
+    };
+  };
+};
