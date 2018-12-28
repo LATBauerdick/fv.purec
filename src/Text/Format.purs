d82d19e28142d0c3a215573b74da5fecf703f674
LATBauerdick <github@latbauerdick.de>
2018-12-28 16:59:58 -0600

    rationalizing strings

--- src/Text/Format.purs
+++ src/Text/Format.purs
@@ -129,8 +129,8 @@ instance formatInt :: Format Int where
 
 instance formatNumber :: Format Number where
   -- Format as an integer if the precision is set to 0
-  format prop@(Properties rec) num | rec.precision == Just 0 =
-    format prop (Int.round num)
+  format prop@(Properties rec) num' | rec.precision == Just 0 =
+    format prop (Int.round num')
 
   format (Properties rec) num' =
     case rec.width of
