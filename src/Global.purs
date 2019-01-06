a9d5ddf71d63d09e12ccbae73338a53596ca7d22
LATBauerdick <github@latbauerdick.de>
2019-01-06 09:27:39 -0600

    work on Global

--- src/Global.purs
+++ src/Global.purs
@@ -3,19 +3,19 @@
 module Global where
 
 -- | Not a number (NaN)
-{-- foreign import nan :: Number --}
+foreign import nan :: Number
 
 -- | Test whether a number is NaN
-{-- foreign import isNaN :: Number -> Boolean --}
+foreign import isNaN :: Number -> Boolean
 
 -- | Positive infinity
-{-- foreign import infinity :: Number --}
+foreign import infinity :: Number
 
 -- | Test whether a number is finite
 foreign import isFinite :: Number -> Boolean
 
 -- | Parse an integer from a `String` in the specified base
-{-- foreign import readInt :: Int -> String -> Number --}
+foreign import readInt :: Int -> String -> Number
 
 -- | Parse a floating point value from a `String`
 foreign import readFloat :: String -> Number
